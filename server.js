c701cb626221a52dbbfee2aac902b75d6864b3fb
AZC <albertzcheng@gmail.com>
2021-02-08 22:45:34 -0800

    html routes works

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const htmlRouter = require("./routes/html-routes");
+const apiRouter = require("./routes/api-routes");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Invoke routes
+htmlRouter(app);
+apiRouter(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.listen(PORT, () => {
+    console.log(`App running at: http://localhost:${PORT}`);
+});
